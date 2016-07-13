690345b78b3586e6a913a5a2972e7b0f8d85b126
sysmy <sy@galacticfog.com>
2016-07-12 19:18:51 -0700

    Minor cleanup.

--- app/controllers/util/MetaController.scala
+++ app/controllers/util/MetaController.scala
@@ -63,6 +63,7 @@ trait MetaController extends SecureController with SecurityResources {
     val protocol = if (request.secure) "https" else "http"
     Some( "%s://%s".format(protocol, request.host) )
   }
+  
   def META_URL(host: String, secure: Boolean = false) = {
     val protocol = if (secure) "https" else "http"
     Some( "%s://%s".format(protocol, host) )
@@ -384,7 +385,6 @@ trait MetaController extends SecureController with SecurityResources {
    */
   protected[controllers] def safeGetInputJson(json: JsValue): Try[GestaltResourceInput] = Try {
     log.debug(s"safeGetInputJson([json]")
-    //log.debug(Json.prettyPrint(json))
     implicit def jsarray2str(arr: JsArray) = arr.toString
 
     json.validate[GestaltResourceInput].map {
