8ba77ac3cae2d40d0b7b33e36ed8da91c7426b3c
sysmy <sy@galacticfog.com>
2016-07-21 20:29:25 -0700

    Reorganize action value objects.

--- app/com/galacticfog/gestalt/meta/auth/Actions.scala
+++ app/com/galacticfog/gestalt/meta/auth/Actions.scala
@@ -1,68 +1,48 @@
 package com.galacticfog.gestalt.meta.auth
 
+
+import java.util.UUID
+import com.galacticfog.gestalt.meta.api.sdk.ResourceIds
+
+
+abstract class ResourceActions(prefix: String) {
+  val Create = s"$prefix.create"
+  val View   = s"$prefix.view"
+  val Update = s"$prefix.update"
+  val Delete = s"$prefix.delete"
+  
+  protected[auth] val actions = Map(
+    "create" -> Create,
+    "view" -> View,
+    "update" -> Update,
+    "delete" -> Delete)
+}
+
 object Actions {
-
-  object Org {
-    private val prefix = "org"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
+   
+  def resourceAction(typeId: UUID, action: String): String = {
+    (typeId match {
+      case ResourceIds.Org => Org
+      case ResourceIds.User => User
+      case ResourceIds.Group => Group
+      case ResourceIds.Workspace => Workspace
+      case ResourceIds.Environment => Environment
+      case ResourceIds.Container => Container
+      case ResourceIds.Lambda => Lambda
+      case ResourceIds.License => License
+      case e => {
+        throw new RuntimeException(s"Unknown Action Object type '$typeId'.")
+      }
+    }).actions(action)
   }
 
-  object User {
-    private val prefix = "user"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }
+  object Org extends ResourceActions("org")
+  object User extends ResourceActions("user")
+  object Group extends ResourceActions("group")
+  object Workspace extends ResourceActions("workspace")
+  object Environment extends ResourceActions("environment")
+  object Lambda extends ResourceActions("lambda")
+  object Container extends ResourceActions("container")
+  object License extends ResourceActions("license")
   
-  object Group {
-    private val prefix = "group"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }
-  
-  object Workspace {
-    private val prefix = "workspace"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }
-
-  object Environment {
-    private val prefix = "environment"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }
-
-  object Lambda {
-    private val prefix = "lambda"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }
-
-  object Container {
-    private val prefix = "container"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }
-  
-  object License {
-    private val prefix = "license"
-    val Create = s"$prefix.create"
-    val View   = s"$prefix.view"
-    val Update = s"$prefix.update"
-    val Delete = s"$prefix.delete"
-  }  
 }
