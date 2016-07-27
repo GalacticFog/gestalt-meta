fb351c7e619944f3919ef207efac8bfda21e57c1
sysmy <sy@galacticfog.com>
2016-07-27 14:24:43 -0700

    Add 'providers' to REST resource name lookup.

--- app/com/galacticfog/gestalt/meta/api/package.scala
+++ app/com/galacticfog/gestalt/meta/api/package.scala
@@ -40,6 +40,7 @@ package object api {
       case "entitlements"     => Some(ResourceIds.Entitlement)
       case "policies"         => Some(ResourceIds.Policy)
       
+      case "providers"        => Some(ResourceIds.Provider)
       
       case "clustertemplates" => Some(ResourceIds.ClusterTemplate)
       case "nodetemplates"    => Some(ResourceIds.NodeTemplate)
