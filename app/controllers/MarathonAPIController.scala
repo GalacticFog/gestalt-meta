5e41fa65d1f8ae61d3bdcfb37aa8c8e2c1a81018
Christopher Baker <cgbaker@gmail.com>
2016-10-13 14:49:41 -0400

    implemented generalized routing for the marathon rest adapter

--- app/controllers/MarathonAPIController.scala
+++ app/controllers/MarathonAPIController.scala
@@ -1,52 +1,27 @@
 package controllers
 
-
 import java.util.UUID
 
-import com.galacticfog.gestalt.marathon._
-import com.galacticfog.gestalt.meta.api.output.Output
 import com.galacticfog.gestalt.security.api.errors.UnauthorizedAPIException
 import org.joda.time.{DateTimeZone, DateTime}
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import scala.concurrent.Future
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
 
-import com.galacticfog.gestalt.data.ResourceFactory
-import com.galacticfog.gestalt.data.models.GestaltResourceInstance
-import com.galacticfog.gestalt.marathon.MarathonClient
 import com.galacticfog.gestalt.meta.api.errors.BadRequestException
-import com.galacticfog.gestalt.meta.api.errors.ResourceNotFoundException
-import com.galacticfog.gestalt.meta.api.errors.ConflictException
-import com.galacticfog.gestalt.meta.api.sdk.ResourceIds
-import com.galacticfog.gestalt.security.play.silhouette.{OrgContextRequestUUID, OrgContextRequest, GestaltFrameworkSecuredController, AuthAccountWithCreds}
-import com.mohiva.play.silhouette.impl.authenticators.DummyAuthenticator
+import com.galacticfog.gestalt.security.play.silhouette.OrgContextRequest
 
 import controllers.util._
-import play.api.Play.current
 import play.api.libs.json._
-import play.api.libs.ws.WS
-import com.galacticfog.gestalt.meta.api.sdk.ResourceLabel
-import com.galacticfog.gestalt.laser._
-import play.api.{Logger => log, Play}
-import scala.concurrent.{ ExecutionContext, Future, Promise, Await }
-import scala.concurrent.duration._
-import com.galacticfog.gestalt.meta.api.output._
-import com.galacticfog.gestalt.events._
-import com.galacticfog.gestalt.meta.policy._
+import scala.concurrent.Future
 import com.galacticfog.gestalt.meta.auth.Authorization
 import com.galacticfog.gestalt.marathon._
-
-import com.galacticfog.gestalt.keymgr.GestaltFeature
-import com.galacticfog.gestalt.meta.auth.Actions
-
 
 class MarathonAPIController(containerService: ContainerService) extends Authorization {
 
   import com.galacticfog.gestalt.security.api.json.JsonImports._
-  import ContainerController.futureToFutureTry
 
   class MarAuth(maybeGenFQON: Option[RequestHeader => Option[String]] = None) extends ActionBuilder[SecuredRequest] {
     def invokeBlock[B](request: Request[B], block: SecuredRequest[B] => Future[Result]) = {
@@ -75,12 +50,12 @@ class MarathonAPIController(containerService: ContainerService) extends Authoriz
   /**
    * GET /{fqon}/environments/{eid}/providers/{pid}/v2/deployments
    */
-  def listDeployments(fqon: String, parentType: String, environment: UUID, providerId: UUID) = MarAuth(fqon).async { implicit request =>
+  def listDeployments(fqon: String, environment: UUID, providerId: UUID) = MarAuth(fqon).async { implicit request =>
     val provider = containerService.marathonProvider(providerId)
-    execAppFunction(fqon, parentType, environment, provider, "v2/deployments") {
-      containerService.marathonClient(provider).listDeploymentsAffectingEnvironment_marathon_v2
-    } map { Ok(_) } recover {
-      case e: Throwable => BadRequest(e.getMessage)
+    containerService.appComponents(environment) match {
+      case Failure(e) => throw e
+      case Success((wrk,env)) =>
+        containerService.marathonClient(provider).listDeploymentsAffectingEnvironment_marathon_v2(fqon, wrk.name, env.name) map { Ok(_) }
     }
   }
 
@@ -96,7 +71,7 @@ class MarathonAPIController(containerService: ContainerService) extends Authoriz
   /**
    * GET /{fqon}/environments/{eid}/providers/{pid}/v2/apps
    */
-  def listApps(fqon: String, parentType: String, environment: UUID, providerId: UUID) = MarAuth(fqon).async { implicit request =>
+  def listApps(fqon: String, environment: UUID, providerId: UUID) = MarAuth(fqon).async { implicit request =>
     containerService.appComponents(environment) match {
       case Failure(e) => throw e
       case Success((wrk,env)) => {
@@ -117,7 +92,7 @@ class MarathonAPIController(containerService: ContainerService) extends Authoriz
   /**
     * GET /{fqon}/environments/{eid}/providers/{pid}/v2/apps/{appId}
     */
-  def getApp(fqon: String, parentType: String, environment: UUID, providerId: UUID, appId: String) = MarAuth(fqon).async { implicit request =>
+  def getApp(fqon: String, environment: UUID, providerId: UUID, appId: String) = MarAuth(fqon).async { implicit request =>
     containerService.appComponents(environment) match {
       case Failure(e) => throw e
       case Success((wrk,env)) => {
@@ -141,7 +116,7 @@ class MarathonAPIController(containerService: ContainerService) extends Authoriz
   /**
     * DELETE /{fqon}/environments/{eid}/providers/{pid}/v2/apps/{appId}
     */
-  def deleteApp(fqon: String, parentType: String, environment: UUID, providerId: UUID, appId: String) = MarAuth(fqon).async { implicit request =>
+  def deleteApp(fqon: String, environment: UUID, providerId: UUID, appId: String) = MarAuth(fqon).async { implicit request =>
     containerService.appComponents(environment) match {
       case Failure(e) => throw e
       case Success((wrk,env)) => {
@@ -167,14 +142,17 @@ class MarathonAPIController(containerService: ContainerService) extends Authoriz
   /**
    * POST /{fqon}/environments/{eid}/providers/{pid}/v2/apps
    */
-  def createApp(fqon: String, parentType: String, environment: UUID, providerId: UUID) = MarAuth(fqon).async(parse.json) { implicit request =>
+  def createApp(fqon: String, environment: UUID, providerId: UUID) = MarAuth(fqon).async { implicit request =>
     containerService.appComponents(environment) match {
       case Failure(e) => throw e
       case Success((wrk,env)) => {
         val provider = containerService.marathonProvider(providerId)
         // TODO: Needs a lot of error handling
         for {
-          (name,props) <- Future.fromTry(marAppPayloadToMetaContainerSpec(request.body, provider) flatMap {
+          body <- Future.fromTry {
+            request.body.asJson.fold[Try[JsValue]](Failure(new RuntimeException("requires json body")))(Success(_))
+          }
+          (name,props) <- Future.fromTry(marAppPayloadToMetaContainerSpec(body, provider) flatMap {
             case (maybeName,cspec) => maybeName match {
               case None => Failure(BadRequestException("payload did not include app name"))
               case Some(name) => Success((name,cspec))
@@ -191,31 +169,15 @@ class MarathonAPIController(containerService: ContainerService) extends Authoriz
     }
   }
 
-  private def execAppFunction(fqon: String,
-                              parentType: String,
-                              environment: UUID,
-                              provider: GestaltResourceInstance, proxyUri: String)
-                             (fn: ProxyAppFunction) =
-  {
-    containerService.appComponents(environment) match {
-      case Failure(e) => throw e
-      case Success((parent, child)) => proxyUri match {
-        case "v2/apps" =>        { fn(fqon, parent.name, child.name) }
-        case "v2/deployments" => { fn(fqon, parent.name, child.name) }
-        case e         => throw new BadRequestException(s"Unsupported Marathon URL : " + e)
-      }
-    }
-  }
-
-  def handleMarathonAPIRequest(method: String, fqon: String, parentType: String, environment: UUID, providerId: UUID, path: String) = MarAuth(fqon).async { implicit request =>
+  def handleRequest(method: String, fqon: String, envId: UUID, providerId: UUID, path: String) = Action.async { implicit request =>
     val AppPath = "apps/(.*)".r
     (method,path) match {
-      case ("GET","apps") => ???             // controllers.MarathonController.getMarathonApps(fqon: String, parentType = "46b28076-9d94-4860-9aca-e8483a56d32c", envId: java.util.UUID, id: java.util.UUID)
-      case ("POST","apps") => ???            // controllers.MarathonController.postMarathonAppDCOS(fqon: String, parentType = "46b28076-9d94-4860-9aca-e8483a56d32c", envId: java.util.UUID, id: java.util.UUID)
-      case ("GET", AppPath(appId)) => ???    // controllers.MarathonController.getMarathonApp(fqon: String, parentType = "46b28076-9d94-4860-9aca-e8483a56d32c", envId: java.util.UUID, id: java.util.UUID, appId: String)
-      case ("GET", "info") => ???            // controllers.MarathonController.getInfo(fqon: String, envId: java.util.UUID, id: java.util.UUID)
-      case ("GET", "deployments") => ???     // controllers.MarathonController.getDeployments(fqon: String, parentType = "46b28076-9d94-4860-9aca-e8483a56d32c", envId: java.util.UUID, id: java.util.UUID)
-      case ("DELETE", AppPath(appId)) => ??? // controllers.MarathonController.deleteMarathonAppDcos(fqon: String, parentType = "46b28076-9d94-4860-9aca-e8483a56d32c", envId: java.util.UUID, prvId: java.util.UUID, marathonAppId: String)
+      case ("GET", "apps")            => listApps(fqon, envId, providerId)(request)
+      case ("POST","apps")            => createApp(fqon, envId, providerId)(request)
+      case ("GET", AppPath(appId))    => getApp(fqon, envId, providerId, appId)(request)
+      case ("GET", "info")            => getInfo(fqon, envId, providerId)(request)
+      case ("GET", "deployments")     => listDeployments(fqon, envId, providerId)(request)
+      case ("DELETE", AppPath(appId)) => deleteApp(fqon, envId, providerId, appId)(request)
     }
   }
 
