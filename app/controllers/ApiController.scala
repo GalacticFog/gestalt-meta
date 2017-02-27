473ee9374e919bd24394ee25a2a5aeedddbb6aaa
sysmy <sy@galacticfog.com>
2017-02-26 22:03:47 -0800

    Add endpoints for creating apis and apiendpoints in Meta

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -54,12 +54,108 @@ class ApiController @Inject()(
     env: GestaltSecurityEnvironment[AuthAccountWithCreds,DummyAuthenticator])
       extends SecureController(messagesApi = messagesApi, env = env) with Authorization {
   
+  case class EndpointImpl(`type`: String, id: String, function: String)
+  object EndpointImpl {
+    implicit lazy val endpointImplFormat = Json.format[EndpointImpl]  
+  }
+  
   def postApi(fqon: String, parent: UUID) = Authenticate(fqon).async(parse.json) { implicit request =>
-    createResourceCommon(fqid(fqon), parent, ResourceIds.Api, request.body)
+    ResourceFactory.findById(ResourceIds.Environment, parent).fold {
+      Future(ResourceNotFound(ResourceIds.Environment, parent))
+    }{ _ =>
+      val payload = validateNewApi(request.body)
+      createResourceCommon(fqid(fqon), parent, ResourceIds.Api, payload)
+    }
+  }
+  
+  private[controllers] def validateNewApi(js: JsValue): JsValue = {
+
+    // provider.id property exists
+    val pid = Js.find(js.as[JsObject], "/properties/provider/id") getOrElse {
+      unprocessable("Missing required property [properties.provider.id]")
+    }
+    
+    // 'provider.id' is valid UUID
+    val uid = parseUUID(pid.as[String]) getOrElse {
+      unprocessable(s"Invalid [provider.id] (not a valid UUID). found '${pid}'")
+    }
+    
+    // 'provider.id' is an existing gateway-manager provider.
+    ResourceFactory.findById(ResourceIds.GatewayManager, uid).fold {
+      throw new ResourceNotFoundException(s"GatewayManager provider with ID '$pid' not found")
+    }{
+      _ => js
+    }
   }
   
   def putApi(fqon: String, api: UUID) = Authenticate(fqon).async(parse.json) { implicit request =>
     ???
+  }
+  
+  
+  def postApiEndpoint(fqon: String, api: UUID) = Authenticate(fqon).async(parse.json) { implicit request =>
+    /*
+     * TODO: Ensure API exists
+     */
+    ResourceFactory.findById(ResourceIds.Api, api).fold {
+      Future(ResourceNotFound(ResourceIds.Api, api)) 
+    }{ a =>
+      /*
+       * TODO: validate endpoint payload
+       */
+      val payload = validateNewEndpoint(request.body)
+      
+      /*
+       * Create in Meta
+       * Create in gestalt-api-gateway
+       * return
+       */
+      createResourceCommon(fqid(fqon), api, ResourceIds.ApiEndpoint, payload)
+    }
+  }
+  
+  def postApiEndpointEnv(fqon: String, env: UUID, api: UUID) = Authenticate(fqon).async(parse.json) { implicit request =>
+    /*
+     * TODO: Ensure environment and api exists.
+     */
+    ResourceFactory.findById(ResourceIds.Api, api).fold {
+      Future(ResourceNotFound(ResourceIds.Api, api)) 
+    }{ a =>
+      createResourceCommon(fqid(fqon), api, ResourceIds.ApiEndpoint, request.body)
+    }
+  }  
+  
+  private[controllers] def postApiEndpointCommon(fqon: String, api: UUID)(implicit request: SecuredRequest[JsValue]) = {
+    
+  }
+  
+  private[controllers] def validateNewEndpoint(js: JsValue): JsValue = {
+
+    val implobj = Js.find(js.as[JsObject], "/properties/implementation") getOrElse {
+      unprocessable("Required value [properties.implementation] is missing.")
+    }
+
+    Js.parse[EndpointImpl](implobj) match {
+      case Failure(e) => unprocessable(s"Malformed [implementation] JSON. found: '${implobj}'")
+      case Success(impl) => {
+
+        // 'type' == lambda
+        if (impl.`type`.toLowerCase != "lambda")
+          unprocessable(s"Unsupported [implementation.type]. found: '${impl.`type`}'")
+
+        // 'id' is valid UUID
+        val implid = parseUUID(impl.id) getOrElse {
+          unprocessable(s"Invalid [implementation.id] (not a valid UUID). found '${impl.id}'")
+        }
+        
+        // 'id' points to existing lambda
+        ResourceFactory.findById(ResourceIds.Lambda, implid).fold {
+          throw new ResourceNotFoundException(s"Lambda with ID '${implid}' not found.")
+        }{ 
+          lmb => js
+        }
+      }
+    }
   }
   
   private[this] def standardRequestOptions(
@@ -83,6 +179,8 @@ class ApiController @Inject()(
       controllers.util.EventsPost(action))
   }
   
+  private[this] def unprocessable(message: String) =
+    throw new UnprocessableEntityException(message)  
 }
 
 
