964704183cdb31339bc380511c45d13bfb27cd04
sysmy <sy@galacticfog.com>
2017-03-28 07:21:33 -0700

    Add option to cleanup containers in caas on bootstrap

--- app/controllers/BootstrapController.scala
+++ app/controllers/BootstrapController.scala
@@ -4,6 +4,7 @@ import scala.util.Failure
 import scala.util.Success
 import com.galacticfog.gestalt.data.bootstrap.Bootstrap
 import com.galacticfog.gestalt.data.uuid2string
+import com.galacticfog.gestalt.data.ResourceFactory
 import com.galacticfog.gestalt.meta.api.sdk.ResourceIds
 import com.galacticfog.gestalt.meta.api.sdk.ResourceOwnerLink
 import controllers.util.{SecureController, Security, trace}
@@ -20,18 +21,36 @@ import com.galacticfog.gestalt.meta.providers._
 @Singleton
 class BootstrapController @Inject()( messagesApi: MessagesApi,
                                      env: GestaltSecurityEnvironment[AuthAccountWithCreds,DummyAuthenticator],
-                                     providerManager: ProviderManager )
+                                     providerManager: ProviderManager,
+                                     deleteController: DeleteController)
   extends SecureController(messagesApi = messagesApi, env = env) with Authorization {
   
   def initProviders() = Authenticate() { implicit request =>
     val results = providerManager.loadProviders()
-    
+
     Ok("TESTING PROVIDER LOADING...")
   }
   
   def bootstrap() = Authenticate() { implicit request =>
     trace("bootstrap()")
     
+    val clean = (request.queryString.contains("clean") && 
+        request.queryString("clean")(0) == "true")
+    
+    if (clean) {
+      log.info("Cleaning up containers...")
+      val containers = ResourceFactory.findAll(ResourceIds.Container)
+      if (containers.isEmpty) {
+        log.info("No containers to clean - proceeding with bootstrap...")
+      } else {
+        containers foreach { c =>
+          log.info(s"Deleting Container: ${c.id}, ${c.name}...")
+          deleteController.manager.delete(c, request.identity, force = true)
+        }
+        log.info("Container deletes complete - proceeding with bootstrap...")
+      }
+    }
+
     log.debug("Looking up root Org in gestalt-security...")
     val rootOrgId = Security.getRootOrg(request.identity)(this.securityClient) match {
       case Success(org) => org.id
