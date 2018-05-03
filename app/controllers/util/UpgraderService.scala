b3b3c34ac581b9ccc0fc3b5f9af271e910e07c16
Chris Baker <chris@cgbaker.net>
2018-05-03 16:21:27 -0400

    wip: testing of UpgradeService

--- app/controllers/util/UpgraderService.scala
+++ app/controllers/util/UpgraderService.scala
@@ -4,53 +4,106 @@ import java.util.UUID
 
 import actors.SystemConfigActor
 import akka.actor.ActorRef
-import javax.inject.{Inject, Named}
-import play.api.libs.json.{Format, JsValue, Json}
 import akka.pattern.ask
+import com.galacticfog.gestalt.data.ResourceFactory
+import com.galacticfog.gestalt.data.models.GestaltResourceInstance
+import com.galacticfog.gestalt.meta.api.Resource
+import com.galacticfog.gestalt.meta.api.sdk.ResourceIds
+import com.galacticfog.gestalt.meta.auth.AuthorizationMethods
+import com.galacticfog.gestalt.meta.providers.{ProviderManager, ProviderMap}
+import javax.inject.{Inject, Named}
+import play.api.libs.json.{Format, JsObject, Json}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
 trait UpgraderService {
   import UpgraderService._
 
-  def deleteUpgrader(creator: UUID, status: UpgradeStatus)
+  def deleteUpgrader(creator: AccountLike, status: UpgradeStatus)
                     (implicit ec: ExecutionContext): Future[UpgradeStatus]
 
-  def launchUpgrader(creator: UUID, payload: UpgradeLaunch)
+  def launchUpgrader(creator: AccountLike, payload: UpgradeLaunch)
                     (implicit ec: ExecutionContext): Future[UpgradeStatus]
 }
 
-class DefaultUpgraderService @Inject() (@Named(SystemConfigActor.name) configActor: ActorRef)
-  extends UpgraderService {
+class DefaultUpgraderService @Inject() ( @Named(SystemConfigActor.name) configActor: ActorRef,
+                                         providerManager: ProviderManager,
+                                         gatewayMethods: GatewayMethods )
+  extends UpgraderService with JsonInput with AuthorizationMethods {
 
   import UpgraderService._
   implicit val askTimeout: akka.util.Timeout = 15.seconds
 
-  override def deleteUpgrader(creator: UUID, status: UpgradeStatus)
+  lazy val rootId = Resource.findFqon("root").map(_.id).getOrElse(throw new RuntimeException("could not find root org"))
+
+  override def deleteUpgrader(creator: AccountLike, status: UpgradeStatus)
                              (implicit ec: ExecutionContext): Future[UpgradeStatus] = {
     // FINISH
     val newStatus = UpgradeStatus.inactive
-    updateStatus(creator, newStatus)
+    updateStatus(creator.id, newStatus, None)
   }
 
-  override def launchUpgrader(creator: UUID, payload: UpgradeLaunch)
+  override def launchUpgrader(creator: AccountLike, payload: UpgradeLaunch)
                              (implicit ec: ExecutionContext): Future[UpgradeStatus] = {
-    // FINISH
-    val newStatus = UpgradeStatus(
-      active = true,
-      endpoint = Some("https://gtw1.test.galacticfog.com/test-upgrader")
-    )
-    updateStatus(creator, newStatus)
+    for {
+      providerProto <- Future.fromTry(
+        jsonToResource(rootId, creator, getProviderPayload(payload), Some(ResourceIds.Provider))
+      )
+      provider <- Future.fromTry(ResourceFactory.create(ResourceIds.User, creator.id)(
+        r = providerProto,
+        parentId = Some(rootId)
+      ))
+      _ <- Future.fromTry(
+        // parent=None means no inheritance, granting entitlements only to creator
+        Try(setNewResourceEntitlements(
+          org = rootId,
+          resource = provider.id,
+          creator = creator,
+          parent = None
+        ).map(_.get))
+      )
+      (_, Seq(container)) <- providerManager.processProvider(ProviderMap(provider))
+      env = providerManager.getOrCreateProviderEnvironment(provider, creator)
+      apiProto <- Future.fromTry(
+        jsonToResource(rootId, creator, getApiPayload(payload), Some(ResourceIds.Api))
+      )
+      api <- Future.fromTry(ResourceFactory.create(ResourceIds.User, creator.id)(
+        r = apiProto,
+        parentId = Some(env.id)
+      ))
+      endpointJson = getEndpointPayload(api, container, payload)
+      endpointProto <- Future.fromTry(
+        jsonToResource(rootId, creator, endpointJson, Some(ResourceIds.ApiEndpoint))
+      )
+      endpointNative <- Future.fromTry(GatewayMethods.toGatewayEndpoint(endpointJson, api.id))
+      endpoint <- Future.fromTry(ResourceFactory.create(ResourceIds.User, creator.id)(
+        r = endpointProto,
+        parentId = Some(api.id)
+      ))
+      apiEndpoint <- gatewayMethods.createEndpoint(
+        api = api,
+        metaEndpoint = endpoint,
+        gatewayEndpoint = endpointNative
+      )
+      publicUrl = GatewayMethods.getPublicUrl(apiEndpoint)
+      newStatus = UpgradeStatus(
+        active = true,
+        endpoint = publicUrl
+      )
+      status <- updateStatus(creator.id, newStatus, Some(provider.id))
+    } yield status
   }
 
-  private[this] def updateStatus(creator: UUID, newStatus: UpgradeStatus)
+  private[this] def updateStatus(creator: UUID, newStatus: UpgradeStatus, providerId: Option[UUID])
                                 (implicit ec: ExecutionContext): Future[UpgradeStatus] = {
     (configActor ? SystemConfigActor.SetKeys(
       creator = creator,
       pairs = Map(
         "upgrade_status" -> Some(Json.toJson(newStatus).toString),
-        "upgrade_lock" -> Some(newStatus.active.toString)
+        "upgrade_lock" -> Some(newStatus.active.toString),
+        "upgrade_provider" -> providerId.map(_.toString)
       )
     )).map { _ => newStatus }
   }
@@ -59,11 +112,98 @@ class DefaultUpgraderService @Inject() (@Named(SystemConfigActor.name) configAct
 
 case object UpgraderService {
 
+  def getEndpointPayload(api: GestaltResourceInstance, container: GestaltResourceInstance, launchPayload: UpgradeLaunch): JsObject = Json.obj(
+    "id" -> java.util.UUID.randomUUID().toString,
+    "name" -> "upgrader",
+    "properties" -> Json.obj(
+      "parent" -> api.id.toString,
+      "location_id" -> launchPayload.kongProviderId.toString,
+      "implementation_type" -> "container",
+      "implementation_id" -> container.id.toString,
+      "container_port_name" -> "api",
+      "methods" -> Seq("GET", "POST"),
+      "resource" -> "upgrader",
+      "provider" -> api.properties.get("provider")
+    )
+  )
+
+  def getApiPayload(launchPayload: UpgradeLaunch): JsObject = Json.obj(
+    "id" -> java.util.UUID.randomUUID().toString,
+    "name" -> java.util.UUID.randomUUID().toString,
+    "properties" -> Json.obj(
+      "provider" -> Json.obj(
+        "id" -> launchPayload.gwmProviderId.toString,
+        "locations" -> Seq(launchPayload.kongProviderId.toString)
+      )
+    )
+  )
+
+  def getProviderPayload(launchPayload: UpgradeLaunch): JsObject = Json.obj(
+    "id" -> java.util.UUID.randomUUID().toString,
+    "name" -> "gestalt-upgrade",
+    "properties" -> Json.obj(
+      "config" -> Json.obj(
+        "auth" -> Json.obj(),
+        "env" -> Json.obj(
+          "private" -> Json.obj(),
+          "public" -> Json.obj()
+        )
+      ),
+      "linked_providers" -> Seq(
+        Json.obj(
+          "id" -> launchPayload.dbProviderId,
+          "name" -> "DB"
+        ),
+        Json.obj(
+          "id" -> launchPayload.secProviderId,
+          "name" -> "SEC"
+        ),
+        Json.obj(
+          "id" -> launchPayload.caasProviderId,
+          "name" -> "CAAS"
+        )
+      ),
+      "services" -> Seq(Json.obj(
+        "container_spec" -> Json.obj(
+          "name" -> "upgrader",
+          "properties" -> Json.obj(
+            "container_type" -> "DOCKER",
+            "cpus" -> 1.0,
+            "force_pull" -> true,
+            "image" -> launchPayload.image,
+            "memory" -> 512,
+            "network" -> "BRIDGE",
+            "num_instances" -> 1,
+            "port_mappings" -> Seq(Json.obj(
+              "container_port" -> 9000,
+              "expose_endpoint" -> true,
+              "name" -> "api",
+              "protocol" -> "tcp"
+            )),
+            "provider" -> Json.obj(
+              "id" -> launchPayload.caasProviderId
+            ),
+            "volumes" -> Seq(Json.obj(
+              "container_path" -> "persistence",
+              "persistent" -> Json.obj("size" -> launchPayload.persistenceSize.getOrElse[Long](1024))
+            ))
+          )
+        ),
+        "init" -> Json.obj(
+          "binding" -> "eager",
+          "singleton" -> true
+        )
+      ))
+    )
+  )
+
   case class UpgradeLaunch( image: String,
                             dbProviderId: UUID,
                             secProviderId: UUID,
                             caasProviderId: UUID,
-                            kongProviderId: UUID )
+                            gwmProviderId: UUID,
+                            kongProviderId: UUID,
+                            persistenceSize: Option[Long] = None)
 
   case class UpgradeStatus(active: Boolean, endpoint: Option[String])
   case object UpgradeStatus {
