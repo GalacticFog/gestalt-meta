39de0cfb952ec39354f551ff7ed1a4ea3573d790
Pavel <pavel.utkov.ctr@galacticfog.com>
2018-11-15 00:16:01 +0300

    Fix to migrations.V21Spec

--- meta/test/migrations/V21Spec.scala
+++ meta/test/migrations/V21Spec.scala
@@ -2,7 +2,7 @@ package migrations
 
 import com.galacticfog.gestalt.data._
 // import com.galacticfog.gestalt.data.models.GestaltTypeProperty
-// import com.galacticfog.gestalt.meta.api.sdk.ResourceIds
+import com.galacticfog.gestalt.meta.api.sdk.ResourceIds
 import com.galacticfog.gestalt.meta.test.MetaRepositoryOps
 import play.api.test.PlaySpecification
 
@@ -10,7 +10,7 @@ class V21Spec extends PlaySpecification with MetaRepositoryOps {
   "V21" >> {
 
     "create AWS Lambda resource type" >> {
-      val tpe = TypeFactory.findById(V20.AWS_LAMBDA_PROVIDER_TYPE_ID)
+      val tpe = TypeFactory.findById(ResourceIds.Lambda)
       PropertyFactory.findByName(tpe.get.id, "aws_role_id") must beSome
       PropertyFactory.findByName(tpe.get.id, "aws_function_id") must beSome
     }
